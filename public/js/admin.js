782878571ed2523b99e122eba34ac292d0989dd9
Vadim <denba2013@yandex.ru>
2019-10-13 20:59:17 +0300

    Add edit functions

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,24 +1,54 @@
 const form = document.querySelector('#post')
+const edit = document.querySelector('#edit')
 const csrf = document.querySelector('#csrf').dataset.csrf
 
-form.addEventListener('submit',event=>{
-    event.preventDefault()
-    const data = new FormData(form)
-
-    axios({
-        method:'POST',
-        url:'/posts/create',
-        headers:{
-            'X-XSRF-TOKEN': csrf
-        },
-        data:data
-    }).then(response => {
-        if(response.data[0] === 'success'){
-            setTimeout(()=>{
-                document.location.href = '/'
-            },1500)
-        }else {
-
-        }
+if(form){
+    form.addEventListener('submit',event=>{
+        event.preventDefault()
+        const data = new FormData(form)
+    
+        axios({
+            method:'POST',
+            url:'/posts/create',
+            headers:{
+                'X-XSRF-TOKEN': csrf
+            },
+            data:data
+        }).then(response => {
+            if(response.data[0] === 'success'){
+                setTimeout(()=>{
+                    document.location.href = '/'
+                },1500)
+            }else {
+    
+            }
+        })
     })
-})
+}
+
+if(edit){
+    edit.addEventListener('submit',event=>{
+        event.preventDefault()
+        const data = new FormData(edit)
+        const id = document.querySelector('#title').dataset.id
+    
+        axios({
+            method:'POST',
+            url:`/posts/edit/${id}`,
+            headers:{
+                'X-XSRF-TOKEN': csrf
+            },
+            data:data
+        }).then(response => {
+            if(response.data[0] === 'success'){
+                setTimeout(()=>{
+                    document.location.href = '/'
+                },1500)
+            }else {
+    
+            }
+        })
+    })
+}
+
+
